ecad8ba64a9ae575d34778a6c11bd574253fc776
janosoto <alejandro.soto@usach.cl>
2015-05-02 04:09:12 -0300

    Arreglado bug de problemas de origen de archivos desde SIAC. Se permite utilizar archivos csv

--- easy-planning-web/src/main/java/managedbeans/CargarPlanDeEstudios.java
+++ easy-planning-web/src/main/java/managedbeans/CargarPlanDeEstudios.java
@@ -7,8 +7,10 @@ package managedbeans;
 
 import business.AsignaturasLocal;
 import entities.Asignatura;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -28,6 +30,7 @@ import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.NotOLE2FileException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.primefaces.model.UploadedFile;
 import sessionbeans.AsignaturaFacadeLocal;
@@ -100,97 +103,167 @@ public class CargarPlanDeEstudios implements Serializable {
         
         String[] aux2 = ruta.split("=", 5);
         //aux = aux2[2].split(", size")[0];
-        aux = nombrePlan;
+        //aux = nombrePlan;
         
-        FileInputStream file = new FileInputStream(aux2[2].split(", size")[0]);
-        workbook = new HSSFWorkbook(file);
-        HSSFSheet sheet = workbook.getSheetAt(0);
-        
-        Iterator rows = sheet.rowIterator();
-        while (rows.hasNext()) {
-            HSSFRow row = (HSSFRow) rows.next();
-            Iterator cells = row.cellIterator();
-            List data = new ArrayList();
-            while (cells.hasNext()) {
-            HSSFCell cell = (HSSFCell) cells.next();
-            data.add(cell);
+        try{
+            FileInputStream file = new FileInputStream(aux2[2].split(", size")[0]);
+            workbook = new HSSFWorkbook(file);
+            HSSFSheet sheet = workbook.getSheetAt(0);
+
+            Iterator rows = sheet.rowIterator();
+            while (rows.hasNext()) {
+                HSSFRow row = (HSSFRow) rows.next();
+                Iterator cells = row.cellIterator();
+                List data = new ArrayList();
+                while (cells.hasNext()) {
+                HSSFCell cell = (HSSFCell) cells.next();
+                data.add(cell);
+                }
+                sheetData.add(data);
             }
-            sheetData.add(data);
-        }
-        
-        for (Object sheetData1 : sheetData) {
-            Asignatura asignatura = new Asignatura();
-            List list = (List) sheetData1;
-            Cell cell = (Cell) list.get(0);
-            int num = (int) cell.getNumericCellValue();
-            asignatura.setCodigo(num+"");
-            cell = (Cell) list.get(1);
-            asignatura.setNombre(cell.getStringCellValue());
-            cell = (Cell) list.get(2);
-            asignatura.setTeoria((int) cell.getNumericCellValue());
-            cell = (Cell) list.get(3);
-            asignatura.setEjercicios((int) cell.getNumericCellValue());
-            cell = (Cell) list.get(4);
-            asignatura.setLaboratorio((int) cell.getNumericCellValue());
-            cell = (Cell) list.get(5);
-            asignatura.setNivel((int) cell.getNumericCellValue());
-            asignatura.setPlanEstudio(nombrePlan);
-            cell = (Cell) list.get(6);
-            /*if(cell.getStringCellValue().equals("Ingreso") || cell.getStringCellValue().equals("")){
-                asignatura.setPrerequisitos(new ArrayList());
-            }
-            else{
-                String[] prerequisitos = cell.getStringCellValue().split(",");
-                List lista = new ArrayList<>();
-                lista.addAll(Arrays.asList(prerequisitos)); 
-                asignatura.setPrerequisitos(lista);
-            }*/
-            try{
-                switch(cell.getCellType()){
-                    case Cell.CELL_TYPE_STRING:{
-                        if(cell.getStringCellValue().equals("Ingreso") || cell.getStringCellValue().equals("")){
-                            asignatura.setPrerequisitos(new ArrayList());
-                        }
-                        else {
-                            String[] prerequisitos = cell.getStringCellValue().split(", "); 
-                            //aux = Arrays.toString(prerequisitos);
-                            if(prerequisitos.length > 1)
-                                aux = prerequisitos[0];
-                            //aux = cell.getStringCellValue() + " - Celda de tipo String";
-                            List lista = new ArrayList();
-                            //for (String prerequisito : prerequisitos) {
-                            for (int i=0; i<prerequisitos.length; i++){
-                                lista.add(getBusiness().findByCodigoAndPlan(prerequisitos[i], nombrePlan));
-                                //aux = getAsignatura(prerequisito).toString();
-                                //lista.add(getBusiness().findByCodigo(prerequisito));
+
+            for (Object sheetData1 : sheetData) {
+                Asignatura asignatura = new Asignatura();
+                List list = (List) sheetData1;
+                Cell cell = (Cell) list.get(0);
+                int num = (int) cell.getNumericCellValue();
+                asignatura.setCodigo(num+"");
+                cell = (Cell) list.get(1);
+                asignatura.setNombre(cell.getStringCellValue());
+                cell = (Cell) list.get(2);
+                asignatura.setTeoria((int) cell.getNumericCellValue());
+                cell = (Cell) list.get(3);
+                asignatura.setEjercicios((int) cell.getNumericCellValue());
+                cell = (Cell) list.get(4);
+                asignatura.setLaboratorio((int) cell.getNumericCellValue());
+                cell = (Cell) list.get(5);
+                asignatura.setNivel((int) cell.getNumericCellValue());
+                asignatura.setPlanEstudio(nombrePlan);
+                cell = (Cell) list.get(6);
+                /*if(cell.getStringCellValue().equals("Ingreso") || cell.getStringCellValue().equals("")){
+                    asignatura.setPrerequisitos(new ArrayList());
+                }
+                else{
+                    String[] prerequisitos = cell.getStringCellValue().split(",");
+                    List lista = new ArrayList<>();
+                    lista.addAll(Arrays.asList(prerequisitos)); 
+                    asignatura.setPrerequisitos(lista);
+                }*/
+                try{
+                    switch(cell.getCellType()){
+                        case Cell.CELL_TYPE_STRING:{
+                            if(cell.getStringCellValue().equals("Ingreso") || cell.getStringCellValue().equals("")){
+                                asignatura.setPrerequisitos(new ArrayList());
                             }
-                            //lista.addAll(Arrays.asList(prerequisitos)); 
-                            asignatura.setPrerequisitos(lista);
+                            else {
+                                String[] prerequisitos = cell.getStringCellValue().split(", "); 
+                                //aux = Arrays.toString(prerequisitos);
+                                //if(prerequisitos.length > 1)
+                                    //aux = prerequisitos[0];
+                                //aux = cell.getStringCellValue() + " - Celda de tipo String";
+                                List lista = new ArrayList();
+                                //for (String prerequisito : prerequisitos) {
+                                for (int i=0; i<prerequisitos.length; i++){
+                                    lista.add(getBusiness().findByCodigoAndPlan(prerequisitos[i], nombrePlan));
+                                    //aux = getAsignatura(prerequisito).toString();
+                                    //lista.add(getBusiness().findByCodigo(prerequisito));
+                                }
+                                //lista.addAll(Arrays.asList(prerequisitos)); 
+                                asignatura.setPrerequisitos(lista);
+                            }
+                            break;
                         }
-                        break;
-                    }
-                    case Cell.CELL_TYPE_NUMERIC:{
-                        List lista = new ArrayList();
-                        //lista.add(getAsignatura((int) cell.getNumericCellValue()+""));
-                        //aux = getBusiness().findByCodigo((int) cell.getNumericCellValue() +"").toString();
-                        aux = (int) cell.getNumericCellValue() +" - Celda de tipo numérica";
-                        asignatura.setPrerequisitos(lista);
-                        //lista.add((int) cell.getNumericCellValue() +"");
-                        lista.add(getBusiness().findByCodigoAndPlan((int) cell.getNumericCellValue() +"", nombrePlan));
+                        case Cell.CELL_TYPE_NUMERIC:{
+                            List lista = new ArrayList();
+                            //lista.add(getAsignatura((int) cell.getNumericCellValue()+""));
+                            //aux = getBusiness().findByCodigo((int) cell.getNumericCellValue() +"").toString();
+                            //aux = (int) cell.getNumericCellValue() +" - Celda de tipo numérica";
+                            asignatura.setPrerequisitos(lista);
+                            //lista.add((int) cell.getNumericCellValue() +"");
+                            lista.add(getBusiness().findByCodigoAndPlan((int) cell.getNumericCellValue() +"", nombrePlan));
+                        }
                     }
                 }
+                catch(EJBException ex){
+                    Throwable cause = ex.getCause();
+                    if (cause != null) {
+                        aux = cause.getLocalizedMessage();
+                    }
+                }
+                asignaturasAñadidas.add(asignatura);
+                persist(asignatura);
             }
-            catch(EJBException ex){
-                Throwable cause = ex.getCause();
+            aux = "Archivo cargado con éxito";
+            cargados = true;
+        }
+        catch(NotOLE2FileException ex){
+            aux = ex.getMessage();
+            boolean flag = false;
+            String linea;
+            BufferedReader buffer = new BufferedReader(new FileReader(aux2[2].split(", size")[0]));
+            try{
+                int k = 0;
+                while((linea = buffer.readLine()) != null){
+                    k++;
+                    
+                    String[] elementos = linea.split(",");
+                    Asignatura asignatura = new Asignatura();
+                    asignatura.setCodigo(elementos[0]);
+                    try{
+                        asignatura.setNombre(elementos[1]);
+                        asignatura.setTeoria(Integer.parseInt(elementos[2]));
+                        asignatura.setEjercicios(Integer.parseInt(elementos[3]));
+                        asignatura.setLaboratorio(Integer.parseInt(elementos[4]));
+                        asignatura.setNivel(Integer.parseInt(elementos[5]));
+                        asignatura.setPlanEstudio(nombrePlan);
+                        if(elementos[6].equals("") || elementos[6].equals("Ingreso")){
+                            asignatura.setPrerequisitos(new ArrayList());
+                        }
+                        else{
+                            List lista = new ArrayList();
+                            if(elementos.length == 8){
+                                lista.add(getBusiness().findByCodigoAndPlan(elementos[6], nombrePlan));
+                            }
+                            else if(elementos.length == 9){
+                                lista.add(getBusiness().findByCodigoAndPlan(elementos[6].substring(1), nombrePlan));
+                                lista.add(getBusiness().findByCodigoAndPlan(elementos[7].substring(1, elementos[7].length()-1), nombrePlan));
+                            }
+                            else if(elementos.length > 9){
+                                lista.add(getBusiness().findByCodigoAndPlan(elementos[6].substring(1), nombrePlan));
+                                int i = 7;
+                                //aux = "cago todo D:";
+                                while(i<elementos.length-2){
+                                    lista.add(getBusiness().findByCodigoAndPlan(elementos[i].substring(1), nombrePlan));
+                                    i++;
+                                }
+                                lista.add(getBusiness().findByCodigoAndPlan(elementos[i].substring(1, elementos[i].length()-1), nombrePlan));                            
+                            }
+
+                            asignatura.setPrerequisitos(lista);
+                        }
+                    }
+                    catch(ArrayIndexOutOfBoundsException | NumberFormatException ex3){
+                        aux = "El archivo tiene problemas internos de codificación. Por favor, ábralo con su editor de texto, guardelo como un archivo csv o xls y vuelva a intentarlo.";
+                        flag = true;
+                        break;
+                    }
+                    asignaturasAñadidas.add(asignatura);
+                    persist(asignatura);
+                    
+                }
+                if(!flag){
+                    aux = "Archivo cargado con éxito";
+                    cargados = true;
+                }
+            }            
+            catch(EJBException ex2){
+                Throwable cause = ex2.getCause();
                 if (cause != null) {
                     //aux = cause.getLocalizedMessage();
                 }
             }
-            asignaturasAñadidas.add(asignatura);
-            persist(asignatura);
-        }
-        aux = "Archivo cargado con éxito";
-        cargados = true;
+            
+        }        
     }
     
     public void persist(Asignatura asignatura){
