25629c178af17708045c6cdc40f94076669cb05b
linc0306 <chienhung.lin@usach.cl>
2015-05-04 13:04:50 -0300

    -Crear EncuestaController para facilitar la creacion de la encuesta -Crear un resultado.xhtml para ver los resultados de la encuesta -Modificar profesor/encuesta.xthml para ver las asignaturas pasadas y poder elegir cuales desea continuar impartiendo -Crear un acceso a resultado de la encuesta

---
+++ easy-planning-web/src/main/java/managedbeans/EncuestaController.java
@@ -0,0 +1,97 @@
+package managedbeans;
+
+import entities.Asignatura;
+import entities.Encuesta;
+import entities.Checklist;
+
+import sessionbeans.EncuestaFacadeLocal;
+import sessionbeans.AsignaturaFacadeLocal;
+import sessionbeans.ChecklistFacadeLocal;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.EJB;
+
+/**
+ *
+ * @author chien
+ */
+@Named(value = "encuestaController")
+@SessionScoped
+public class EncuestaController implements Serializable {
+    
+    @EJB
+    private EncuestaFacadeLocal encuestaFacade;
+    @EJB
+    private AsignaturaFacadeLocal asignaturaFacade;
+    @EJB
+    private ChecklistFacadeLocal checklistFacade;
+    private List<String> listaContinuidad;    
+    private String comentario;
+    
+    public EncuestaController() {
+    }
+
+    public EncuestaFacadeLocal getFacade() {
+        return encuestaFacade;
+    }
+
+    public AsignaturaFacadeLocal getAsignaturaFacade() {
+        return asignaturaFacade;
+    }
+    
+    public ChecklistFacadeLocal getChecklistFacade() {
+        return checklistFacade;
+    }
+
+    public List<String> getListaContinuidad() {
+        return listaContinuidad;
+    }
+
+    public void setListaContinuidad(List<String> listaContinuidad) {
+        this.listaContinuidad = listaContinuidad;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+    
+    public void crearEncuesta(){
+
+        if(!listaContinuidad.isEmpty()){
+            Encuesta encuesta = new Encuesta();                
+            getFacade().create(encuesta);
+
+            for(int i = 0; i < listaContinuidad.size(); i++){
+                Checklist checklist = new Checklist();
+                getChecklistFacade().create(checklist);
+                Long id = Long.parseLong(listaContinuidad.get(i));
+                Asignatura asignatura = getAsignaturaFacade().find(id);
+                checklist.setEncuesta(encuesta);
+                checklist.setAsignatura(asignatura);
+                getChecklistFacade().edit(checklist);
+            }
+        }
+    }
+    
+    public ArrayList<Asignatura> asignaturasEncuesta(String encuestaid){
+        Long id = Long.parseLong(encuestaid);
+        List<Checklist> aux = getFacade().find(id).getAsignaturasAceptadas();
+        
+        ArrayList<Asignatura> asignaturas = new ArrayList<>();
+        
+        for(Checklist check : aux){
+            asignaturas.add(check.getAsignatura());
+        }
+        
+        return asignaturas;
+    }
+    
+}
