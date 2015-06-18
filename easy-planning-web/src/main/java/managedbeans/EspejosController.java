a8563599122c4b4aadb5fd4933e5695514e7f967
arielMerino <ariel.merino@usach.cl>
2015-06-18 01:49:53 -0300

    seleccionar asignaturas espejo

--- easy-planning-web/src/main/java/managedbeans/EspejosController.java
+++ easy-planning-web/src/main/java/managedbeans/EspejosController.java
@@ -9,12 +9,17 @@ import business.AsignaturasLocal;
 import business.VersionesPlanLocal;
 import entities.Asignatura;
 import entities.VersionPlan;
+import java.io.IOException;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import sessionbeans.AsignaturaFacadeLocal;
+import sessionbeans.VersionPlanFacadeLocal;
 
 /**
  *
@@ -32,18 +37,69 @@ public class EspejosController implements Serializable {
     private VersionesPlanLocal versionesPlanBusiness;
     @EJB
     private AsignaturasLocal asignaturasBusiness;
+    @EJB
+    private AsignaturaFacadeLocal asignaturaFacade;
+    @EJB
+    private VersionPlanFacadeLocal versionPlanFacade;
     /*           *
      * Atributos *
      *           */
     
     private String alias;
     
-    private List<Long> asignaturasSeleccionadas;
+    private Long[] asignaturasSeleccionadas;
     
-    private long idqlo;
     /*         *
      * Métodos *
      *         */ 
+    
+    public void redirigir() throws IOException{
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.getExternalContext().redirect("/easy-planning-web/faces/coordinador_docente/plan_de_estudios/espejos.xhtml");
+    }
+    
+    public void inicializarAsignaturasSeleccionadas(){
+        List<VersionPlan> versiones = versionPlanFacade.findAll();
+        long max = 0;
+        for (VersionPlan v : versiones){
+            if (v.getId() > max)
+                max = v.getId();
+        }
+        System.out.println("max: "+max);
+        this.asignaturasSeleccionadas = new Long[Integer.valueOf(max+"")+1];
+        if (max > 0){
+            System.out.println("super aqui");
+            for (int i = 0; i < max+1; i++){
+                asignaturasSeleccionadas[i] = 0L;
+            }
+        }
+    }
+    
+    public void setEspejos(){
+        for (long l : this.asignaturasSeleccionadas){
+            if (l > 0L){
+                Asignatura a = asignaturaFacade.find(l);
+                a.setAlias(alias);
+                asignaturaFacade.edit(a);
+            }
+        }
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Los espejos han sido seteados correctamente.", null));
+    }
+    
+    public void mostrarSeleccionados(){
+        if(asignaturasSeleccionadas.length > 0){
+            String salida = "";
+            for (int i = 0; i < asignaturasSeleccionadas.length; i++){
+                if(asignaturasSeleccionadas[i] != null)
+                    salida += asignaturasSeleccionadas[i]+", ";
+                else
+                    salida +="null, ";
+            }
+            System.out.println("salida: "+salida);
+        }else{
+            System.out.println("Salida: null");
+        }
+    }
     
     public List<VersionPlan> getVersionesPlanActivas(){
         List<VersionPlan> v = this.versionesPlanBusiness.findByPlanificado(true);
@@ -57,24 +113,41 @@ public class EspejosController implements Serializable {
         List<VersionPlan> versiones = this.versionesPlanBusiness.findByPlanificado(true);
         List<VersionPlan> civil = new ArrayList<>();
         for (VersionPlan v : versiones){
-            if (v.getPlanEstudio().getCarrera().getNombre().equalsIgnoreCase("INGENIERÍA CIVIL INFORMÁTICA"))
+            if (v.getPlanEstudio().getCarrera().getNombre().equalsIgnoreCase("INGENIERÍA CIVIL EN INFORMÁTICA"))
                 civil.add(v);
         }
         return civil;
     }
     
-    public List<VersionPlan> getVersioneseEjecucion(){
+    public List<VersionPlan> getVersionesEjecucion(){
         List<VersionPlan> versiones = this.versionesPlanBusiness.findByPlanificado(true);
         List<VersionPlan> ejecucion = new ArrayList<>();
         for (VersionPlan v : versiones){
-            if (!v.getPlanEstudio().getCarrera().getNombre().equalsIgnoreCase("INGENIERÍA CIVIL INFORMÁTICA"))
+            if (!v.getPlanEstudio().getCarrera().getNombre().equalsIgnoreCase("INGENIERÍA CIVIL EN INFORMÁTICA"))
                 ejecucion.add(v);
         }
         return ejecucion;
     }
     
     public List<Asignatura> asignaturasByPlan(long idVersion){
-        return asignaturasBusiness.findByCarreraAndPlan(idVersion);
+        List<Asignatura> asgs = asignaturaFacade.findAll();
+        List<Asignatura> salida = new ArrayList<>();
+        for (Asignatura a : asgs){
+            if (a.getVersionplan().getId() == idVersion)
+                salida.add(a);
+        }
+        return salida;
+    }
+    
+    public String mostrarAsg(int idVersion){
+        String salida = "";
+        List<Asignatura> asgs = asignaturaFacade.findAll();
+        for (Asignatura a : asgs ){
+            if (a.getVersionplan().getId() == idVersion)
+                salida += a.getNombre()+"<br/>";
+        }
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("hola mundo"));
+        return salida;
     }
     
     /*                   *
@@ -89,22 +162,14 @@ public class EspejosController implements Serializable {
         this.alias = alias;
     }
 
-    public List<Long> getAsignaturasSeleccionadas() {
+    public Long[] getAsignaturasSeleccionadas() {
         return asignaturasSeleccionadas;
     }
 
-    public void setAsignaturasSeleccionadas(List<Long> asignaturasSeleccionadas) {
+    public void setAsignaturasSeleccionadas(Long[] asignaturasSeleccionadas) {
         this.asignaturasSeleccionadas = asignaturasSeleccionadas;
     }
-
-    public long getIdqlo() {
-        return idqlo;
-    }
-
-    public void setIdqlo(long idqlo) {
-        this.idqlo = idqlo;
-    }
-    
+       
     public EspejosController() {
     }
 }
